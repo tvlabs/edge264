689d69a142311240f5fe830c3d7b2728e494fd1c
Thibault Raffaillac <traf@kth.se>
2021-10-27 09:48:12 +0200

    Introducing a function to fetch n bytes from memory

--- edge264_bitstream.c
+++ edge264_bitstream.c
@@ -1,32 +1,8 @@
 #include "edge264_common.h"
 
 
-/**
- * Read Exp-Golomb codes and bit sequences.
- *
- * These critical functions are designed to contain minimal numbers of branches
- * and memory writes. Several approaches were tried before:
- * _ Removing all emulation_prevention_three_bytes beforehand to an
- *   intermediate buffer, then making 1~3 aligned reads. The first part incurs
- *   some complexity for the buffer management, but then the second part is
- *   simple and branchless (3 functions with fixed number of aligned reads for
- *   different maximum code sizes).
- * _ The same as above but with unaligned reads, to make 1 read for most codes.
- *   This part is frustrating, because it is more complex but still faster.
- * _ Removing emulation_prevention_three_bytes on the fly to a uint64_t cache
- *   with a shift count. This approach incurs a test for refill (predicted not
- *   taken), but spares a lot of buffer management code. However it incurs some
- *   complexity to reassemble codes bigger than 32 bits.
- * _ The same as above but with a size_t[2] cache with shift count. On 64-bit
- *   machines it allows reading ANY code size with very few instructions, and
- *   pushes the refill call at the end for tail call optimization.
- * _ The same size_t[2] buffer but with the shift replaced by a trailing set
- *   bit. It requires updating 2 variables instead of 1 (shift), but shortens
- *   all dependency chains and allows storing the whole context in two Global
- *   Register Variables.
- */
 #ifdef __SSSE3__
-noinline int FUNC(refill, int ret)
+static always_inline size_t FUNC(get_bytes, int nbytes)
 {
 	static const uint8_t shr_data[32] = {
 		 0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15,
@@ -65,15 +41,13 @@ noinline int FUNC(refill, int ret)
 	unsigned esc = bytes0 & bytes0 >> 1 & bytes03;
 	
 	// iterate on escape sequences that fall inside the bytes to refill
-	while (__builtin_expect(esc & (SIZE_BIT == 32 ? 0xf : 0xff), 0)) {
+	while (__builtin_expect(esc & ((1 << nbytes) - 1), 0)) {
 		int i = __builtin_ctz(esc);
-		
 		// when hitting a start code, point at the 3rd byte to stall future refills there
 		if (CPB[i] <3) {
 			after = CPB + i;
 			break;
 		}
-		
 		// otherwise this is an emulation_prevention_three_byte -> remove it
 		x = _mm_shuffle_epi8(x, (__m128i)shuf[i]);
 		esc = (esc & (esc - 1)) >> 1;
@@ -81,16 +55,46 @@ noinline int FUNC(refill, int ret)
 		after = after + 1 < end ? after + 1 : end;
 	}
 	
-	// increment CPB and insert the unescaped bytes into the cache
+	// increment CPB and return the request bytes in upper part of the result
 	ctx->CPB = after;
+	return big_endian(((v16u)x)[0]);
+}
+#endif
+
+
+
+/**
+ * Read Exp-Golomb codes and bit sequences.
+ *
+ * These critical functions are designed to contain minimal numbers of branches
+ * and memory writes. Several approaches were tried before:
+ * _ Removing all emulation_prevention_three_bytes beforehand to an
+ *   intermediate buffer, then making 1~3 aligned reads. The first part incurs
+ *   some complexity for the buffer management, but then the second part is
+ *   simple and branchless (3 functions with fixed number of aligned reads for
+ *   different maximum code sizes).
+ * _ The same as above but with unaligned reads, to make 1 read for most codes.
+ *   This part is frustrating, because it is more complex but still faster.
+ * _ Removing emulation_prevention_three_bytes on the fly to a uint64_t cache
+ *   with a shift count. This approach incurs a test for refill (predicted not
+ *   taken), but spares a lot of buffer management code. However it incurs some
+ *   complexity to reassemble codes bigger than 32 bits.
+ * _ The same as above but with a size_t[2] cache with shift count. On 64-bit
+ *   machines it allows reading ANY code size with very few instructions, and
+ *   pushes the refill call at the end for tail call optimization.
+ * _ The same size_t[2] buffer but with the shift replaced by a trailing set
+ *   bit. It requires updating 2 variables instead of 1 (shift), but shortens
+ *   all dependency chains and allows storing the whole context in two Global
+ *   Register Variables.
+ */
+noinline int FUNC(refill, int ret) {
 	int trailing_bit = ctz(msb_cache);
 	int shift = trailing_bit + 1;
-	size_t bytes = big_endian(((v16u)x)[0]);
+	size_t bytes = CALL(get_bytes, SIZE_BIT >> 3);
 	msb_cache = lsd(msb_cache >> shift, bytes, shift);
 	lsb_cache = bytes << shift | (size_t)1 << trailing_bit;
 	return ret;
 }
-#endif
 
 noinline int FUNC(get_u1) {
 	int ret = msb_cache >> (SIZE_BIT - 1);

8e4f190e78947185efd0f35213e94a202b8371e3
Steve Lhomme <robux4@ycbcr.xyz>
2025-01-07 15:51:22 +0100

    disable SSE2 detection with clang-cl
    For some reason it compiles but doesn't link.

--- edge264.c
+++ edge264.c
@@ -111,7 +111,7 @@ Edge264Decoder *edge264_alloc(int n_threads, FILE *trace_headers, FILE *trace_sl
 	dec->taskPics_v = set8(-1);
 	
 	// select parser functions based on CPU capabilities
-	#if defined(__SSE2__) // if compiled for Intel
+	#if defined(__SSE2__) && !defined(_MSC_VER) // if compiled for Intel
 		__builtin_cpu_init();
 		if (!__builtin_cpu_supports("cmov") || !__builtin_cpu_supports("sse2"))
 			return free(dec), NULL;

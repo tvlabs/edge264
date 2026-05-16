486f01097da067b06bfc00db21625b9c8f6f4336
Thibault Raffaillac <traf@ik.me>
2026-05-16 15:09:46 +0200

    Fine-tuning benchmark display

--- misc-scripts/bench-decoders.py
+++ misc-scripts/bench-decoders.py
@@ -20,9 +20,9 @@ with open("test.cfg", "w") as f:
 
 edge264_gcc, edge264_clang, ffmpeg, libavc, openh264 = [], [], [], [], []
 for _ in range(int(sys.argv[2])):
-	edge264_gcc.append(float(run(["edge264_test-gcc", "-by", sys.argv[1]], capture_output=True).stdout.split(b'\n')[-3][5:10]))
 	edge264_clang.append(float(run(["edge264_test-clang", "-by", sys.argv[1]], capture_output=True).stdout.split(b'\n')[-3][5:10]))
+	edge264_gcc.append(float(run(["edge264_test-gcc", "-by", sys.argv[1]], capture_output=True).stdout.split(b'\n')[-3][5:10]))
 	ffmpeg.append(float(run(["ffmpeg", "-hide_banner", "-benchmark", "-threads", "1", "-c:v", "h264", "-i", sys.argv[1], "-f", "null", "-"], capture_output=True).stderr.split(b'\n')[-3][13:18]))
-	libavc.append(timeit(lambda: run("avcdec", capture_output=True), number=1))
 	openh264.append(float(run(["h264dec", sys.argv[1]], capture_output=True).stderr.split(b'\n')[6].split()[2]))
-print(f'{{"edge264-GCC":{median(edge264_gcc):.1f},"edge264-Clang":{median(edge264_clang):.1f},"FFmpeg":{median(ffmpeg):.1f},"LibAVC":{median(libavc):.1f},"OpenH264":{median(openh264):.1f}}}')
+	libavc.append(timeit(lambda: run("avcdec", capture_output=True), number=1))
+print(f'{{"edge264-Clang":{median(edge264_clang):.1f},"edge264-GCC":{median(edge264_gcc):.1f},"FFmpeg":{median(ffmpeg):.1f},"OpenH264":{median(openh264):.1f},"LibAVC":{median(libavc):.1f}}}')
